59eb2c5acefcaddabe3bd91a7336561a316d65f9
AustinRhoads <austinrrhoads@ymail.com>
2020-11-07 22:57:07 -0600

    Monsters controller

---
+++ app/controllers/monsters_controller.rb
@@ -0,0 +1,20 @@
+class MonstersController < ApplicationController
+  def index
+    @monsters = Monster.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
 
+  get 'monsters/index'
+  get 'monsters/show'
+  get 'monsters/new'
+  get 'monsters/create'
+  get 'monsters/show'
+  get 'monsters/destroy'
 resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
