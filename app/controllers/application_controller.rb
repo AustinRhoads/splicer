c246ba535a4453175ff0b1fe472b2a34288c7102
AustinRhoads <austinrrhoads@ymail.com>
2020-12-23 15:49:24 -0600

    main#battles_show and text-shadow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,15 @@ class ApplicationController < ActionController::Base
    helper_method :current_user
    helper_method :logged_in?
    helper_method :check_login
+   helper_method :user_is_self
 
    def current_user
      @current_user ||= User.find(session[:user_id]) if session[:user_id]
      
+   end
+
+   def user_is_self
+     current_user == @user ? true:false
    end
 
    def logged_in?
--- app/views/battles/_battle.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! battle, :id, :created_at, :updated_at
-json.url battle_url(battle, format: :json)
